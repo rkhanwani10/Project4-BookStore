f524024e31b0f5fa56f428041d696342e70d24d0
Rohit Khanwani <Rk632@cornell.edu>
2017-07-17 17:33:21 +0400

    Changed port

--- views.py
+++ views.py
@@ -284,5 +284,5 @@ def editPatient(patient_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
